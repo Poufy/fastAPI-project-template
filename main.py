0d2668884b55e5010a3449296c69b7a552b044ed
Noor <nooraldeenalfayad@hotmail.com>
2022-12-26 03:07:15 +0300

    [feat] add routes for more structure

--- main.py
+++ main.py
@@ -1,57 +1,11 @@
-from fastapi import FastAPI, status, Response
-from enum import Enum
-from typing import Optional
+from fastapi import FastAPI
+from api import blogs, greet
 
 app = FastAPI()
 
+app.include_router(blogs.blogsRouter)
+app.include_router(greet.greetRouter)
 
 @app.get('/')
 def index():
     return {'message': 'Hello World!'}
-
-# Order of the routes matter. The first route that matches the request will be executed. so if you have a route that matches any path, it should be the last route in the file.
-@app.get('/greet/anyone', summary='Greet everyone', description='This is a description for the greet everyone route', response_description='This is the response description')
-def greet():
-    return {'message': 'Hello everyone!'}
-
-
-@app.get('/greet/{name}')
-def greet(name: str):
-    return {'message': f'Hello {name}! How are you doing today?'}
-
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
-def get_blog(id: int, response: Response):
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'error': f'Blog with id: {id} not found'}
-    else:
-        return {'message': f'Blog id is {id}'}
-
-
-# Predefined path parameters
-class BlogType(str, Enum):
-    short = 'short'
-    story = 'story'
-    howto = 'howto'
-
-
-@app.get('/blogs/types/{type}')
-def get_blog_Type(type: BlogType):
-    return {'message': f'Blog type is {type}'}
-
-# Query parameters. All the query parameters are optional.
-# The function parameter name should match the query parameter name.
-@app.get('/blog/all')
-def get_all_blogs(page=1, page_size=20):
-    return {'message': f'Page: {page}, Page Size: {page_size}'}
-
-# Optional query parameters
-@app.get('/blog/optional')
-def get_blogs_optional(page=1, page_size: Optional[int] = None):
-    return {'message': f'Page: {page}, Page Size: {page_size}'}
-
-# Query and path parameters together
-@app.get('/blog/{id}/comments/{comment_id}')
-def get_blog_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
-    return {'message': f'Blog id: {id}, Comment id: {comment_id}, Valid: {valid}, Username: {username}'}
